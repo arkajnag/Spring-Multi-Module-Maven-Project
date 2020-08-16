31a8c1c8d2087b26a1b2dc9804d5a7bfe1a97f07
arkajnag <arka.imps@gmail.com>
2020-08-16 13:26:43 +0200

    Adding Multi-module project with io-car-service and io-user-service

---
+++ io-car-service/src/main/java/io/car/service/IoCarServiceApplication.java
@@ -0,0 +1,30 @@
+package io.car.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/rest/car")
+public class IoCarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoCarServiceApplication.class, args);
+	}
+
+	@GetMapping(value="/start",produces = "application/json")
+	public String startCar() {
+		return "Started Car Service in Thread:"+Thread.currentThread().getName()+" at time:"+LocalDateTime.now();
+	}
+	
+	
+	@GetMapping(value="/stop",produces = "application/json")
+	public String stopCar() {
+		return "Stopped Car Service in Thread:"+Thread.currentThread().getName()+" at time:"+LocalDateTime.now();
+	}
+}
